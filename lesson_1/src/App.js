787f7a719fe950bc3a6ebc867fcc0a79a165cfcc
Vladislav <vladislavsafarov04@mail.ru>
2024-09-15 19:28:58 +0300

    Tic-tac

--- lesson_1/src/App.js
+++ lesson_1/src/App.js
@@ -3,10 +3,10 @@ import './style/Message/Message.css';
 function App() {
   return (
     <div className = "app">
-      <Message name = "Vladislav" mood = "Замечательно"/>
-      <Message name = "Andrew" mood = "Плохо"/>
-      <Message name = "Oleg" mood = "Отлично"/>
-      <Message name = "Sergey" mood = "Средне"/>
+      <Message name = "Владислав" mood = "Замечательно"/>
+      <Message name = "Андрей" mood = "Плохо"/>
+      <Message name = "Олег" mood = "Отлично"/>
+      <Message name = "Сергей" mood = "Средне"/>
     </div>
   );
 }
---
+++ tic-tac/src/App.js
@@ -0,0 +1,12 @@
+import Square from "./components/Square/Square.jsx";
+import "./style/Square/Square.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Square />
+    </div>
+  );
+}
+
+export default App;
